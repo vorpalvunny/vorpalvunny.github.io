b256975fd5a66f36ebfc618a694592c37bc7cb30
vorpalvunny <vorpal.vunny.poker.taipei@gmail.com>
2022-07-13 03:42:24 +0800

    initial commit

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var serviceWorkerOption = {
   "assets": [
     "/css/0.css",
-    "/js/main.3a2e59c09ee684a30318.js",
-    "/js/runtime.3a2e59c09ee684a30318.js",
-    "/js/vendor.3a2e59c09ee684a30318.js",
+    "/js/main.a471b5e8707ec910b30b.js",
+    "/js/runtime.a471b5e8707ec910b30b.js",
+    "/js/vendor.a471b5e8707ec910b30b.js",
     "/manifest.json",
     "/browserconfig.xml",
     "/public/android-chrome-192x192.png",
@@ -11,17 +11,17 @@ var serviceWorkerOption = {
     "/public/favicon-16x16.png",
     "/public/favicon-32x32.png",
     "/public/favicon.ico",
-    "/public/img/page0.jpg",
-    "/public/img/page10.jpg",
-    "/public/img/page1.jpg",
-    "/public/img/page11.jpg",
     "/public/android-chrome-512x512.png",
+    "/public/img/page0.jpg",
+    "/public/img/page1.jpg",
+    "/public/img/page10.jpg",
     "/public/img/page12.jpg",
-    "/public/img/page3.jpg",
+    "/public/img/page11.jpg",
     "/public/img/page13.jpg",
     "/public/img/page2.jpg",
-    "/public/img/page5.jpg",
+    "/public/img/page3.jpg",
     "/public/img/page4.jpg",
+    "/public/img/page5.jpg",
     "/public/img/page6.jpg",
     "/public/img/page7.jpg",
     "/public/img/page8.jpg",
@@ -30,15 +30,15 @@ var serviceWorkerOption = {
     "/public/safari-pinned-tab.svg",
     "/public/ipad_splash.png",
     "/public/ipadpro1_splash.png",
-    "/public/iphone6_splash.png",
     "/public/iphone5_splash.png",
     "/public/ipadpro2_splash.png",
-    "/public/iphonex_splash.png",
     "/public/ipadpro3_splash.png",
+    "/public/iphone6_splash.png",
     "/public/iphoneplus_splash.png",
     "/public/iphonexr_splash.png",
+    "/public/iphonex_splash.png",
     "/public/iphonexsmax_splash.png"
   ]
 };
         
-        !function(e){var n={};function i(p){if(n[p])return n[p].exports;var c=n[p]={i:p,l:!1,exports:{}};return e[p].call(c.exports,c,c.exports,i),c.l=!0,c.exports}i.m=e,i.c=n,i.d=function(e,n,p){i.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:p})},i.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},i.t=function(e,n){if(1&n&&(e=i(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var p=Object.create(null);if(i.r(p),Object.defineProperty(p,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var c in e)i.d(p,c,function(n){return e[n]}.bind(null,c));return p},i.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return i.d(n,"a",n),n},i.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},i.p="",i(i.s="JJ5d")}({JJ5d:function(e,n){var i="".concat("VB_MENU_").concat("1.0.0"),p=["/public/android-chrome-192x192.png","/public/apple-touch-icon.png","/public/favicon-16x16.png","/public/favicon-32x32.png","/public/favicon.ico","/public/android-chrome-512x512.png","/public/img/page0.jpg","/public/img/page1.jpg","/public/img/page10.jpg","/public/img/page11.jpg","/public/img/page13.jpg","/public/img/page2.jpg","/public/img/page3.jpg","/public/img/page12.jpg","/public/img/page4.jpg","/public/img/page7.jpg","/public/img/page6.jpg","/public/img/page5.jpg","/public/img/page9.jpg","/public/img/page8.jpg","/public/manifest.json","/public/safari-pinned-tab.svg","/public/mstile-150x150.png","/public/ipad_splash.png","/public/ipadpro2_splash.png","/public/iphone6_splash.png","/public/iphone5_splash.png","/public/ipadpro1_splash.png","/public/ipadpro3_splash.png","/public/iphoneplus_splash.png","/public/iphonex_splash.png","/public/iphonexr_splash.png","/public/iphonexsmax_splash.png"];self.addEventListener("fetch",(function(e){console.log("fetch request : ".concat(e.request.url)),e.respondWith(caches.match(e.request).then((function(n){return n?(console.log("responding with cache : ".concat(e.request.url)),n):(console.log("file is not cached, fetching : ".concat(e.request.url)),fetch(e.request))})))})),self.addEventListener("install",(function(e){e.waitUntil(caches.open(i).then((function(e){return console.log("installing cache : ".concat(i)),e.addAll(p)})))})),self.addEventListener("activate",(function(e){e.waitUntil(caches.keys().then((function(e){var n=e.filter((function(e){return e.indexOf("VB_MENU_")}));return n.push(i),Promise.all(e.map((function(i,p){if(-1===n.indexOf(i))return console.log("deleting cache : ".concat(e[p])),caches.delete(e[p])})))})))}))}});
+        !function(e){var n={};function p(i){if(n[i])return n[i].exports;var c=n[i]={i:i,l:!1,exports:{}};return e[i].call(c.exports,c,c.exports,p),c.l=!0,c.exports}p.m=e,p.c=n,p.d=function(e,n,i){p.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:i})},p.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},p.t=function(e,n){if(1&n&&(e=p(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var i=Object.create(null);if(p.r(i),Object.defineProperty(i,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var c in e)p.d(i,c,function(n){return e[n]}.bind(null,c));return i},p.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return p.d(n,"a",n),n},p.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},p.p="",p(p.s="JJ5d")}({JJ5d:function(e,n){var p="".concat("VB__MENU__").concat("1.0.0"),i=["/public/android-chrome-192x192.png","/public/apple-touch-icon.png","/public/favicon-16x16.png","/public/favicon-32x32.png","/public/favicon.ico","/public/android-chrome-512x512.png","/public/img/page0.jpg","/public/img/page1.jpg","/public/img/page10.jpg","/public/img/page11.jpg","/public/img/page13.jpg","/public/img/page2.jpg","/public/img/page3.jpg","/public/img/page12.jpg","/public/img/page4.jpg","/public/img/page7.jpg","/public/img/page6.jpg","/public/img/page5.jpg","/public/img/page9.jpg","/public/img/page8.jpg","/public/safari-pinned-tab.svg","/public/mstile-150x150.png","/public/ipad_splash.png","/public/ipadpro2_splash.png","/public/iphone6_splash.png","/public/iphone5_splash.png","/public/ipadpro1_splash.png","/public/ipadpro3_splash.png","/public/iphoneplus_splash.png","/public/iphonex_splash.png","/public/iphonexr_splash.png","/public/iphonexsmax_splash.png"];self.addEventListener("fetch",(function(e){console.log("fetch request : ".concat(e.request.url)),e.respondWith(caches.match(e.request).then((function(n){return n?(console.log("responding with cache : ".concat(e.request.url)),n):(console.log("file is not cached, fetching : ".concat(e.request.url)),fetch(e.request))})))})),self.addEventListener("install",(function(e){e.waitUntil(caches.open(p).then((function(e){return console.log("installing cache : ".concat(p)),e.addAll(i)})))})),self.addEventListener("activate",(function(e){e.waitUntil(caches.keys().then((function(e){var n=e.filter((function(e){return e.indexOf("VB__MENU__")}));return n.push(p),Promise.all(e.map((function(p,i){if(-1===n.indexOf(p))return console.log("deleting cache : ".concat(e[i])),caches.delete(e[i])})))})))}))}});
